51f025a54bfd4ffa5bdabea9771b3f9c07ffecf5
IT26101271 <IT26101271@my.sliit.lk>
2026-09-18 14:40:16 +0530

    IT26101271Lab8Q4.java

---
+++ Q4/IT26101271Lab8Q4.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class IT26101271Lab8Q4 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int[] studentsArray = new int[8];
+
+        int i = 0;
+
+        while (i < 8) {
+
+            System.out.print("Enter Student ID: ");
+            int id = input.nextInt();
+
+            if (id <= 0) {
+                System.out.println("Error! Student ID must be positive.");
+            } else {
+                studentsArray[i] = id;
+                i++;
+            }
+        }
+
+        System.out.print("Enter Student ID to search: ");
+        int searchID = input.nextInt();
+
+        boolean found = false;
+
+        for (i = 0; i < 8; i++) {
+
+            if (studentsArray[i] == searchID) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Student is Available");
+        } else {
+            System.out.println("Student is Not Available");
+        }
+    }
+}
